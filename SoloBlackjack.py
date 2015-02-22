8d663cb3daf67c494ebfaef7983e1ec5c50471d1
chenyike <chenyike.upenn@gmail.com>
2015-02-22 15:04:32 -0500

    Sun.2

--- SoloBlackjack.py
+++ SoloBlackjack.py
@@ -1,73 +1,7 @@
 #author - Yike Chen and Yejia Li
 
 import random  # needed for shuffling a Deck
-
-class Card(object):
-    'Create a class containg the information of the card'
-    #the card has a suit which is one of 'S','C','H' or 'D'
-    #the card has a rank 
-    
-    def __init__(self, r, s):
-        'Initial the class'
-        #implement
-        #where r is the rank, s is suit
-        self.rank = str(r)
-        self.suit = str(s)
-
-    def __str__(self):
-        'Output the string'
-        return 'this is '+ str(self.rank) + str(self.suit)
-
-    def get_rank(self):
-        'Get rank'
-        return self.rank
-
-    def get_suit(self):
-        'Get suit'
-        return self.suit
-
-
-class Deck():
-    """Denote a deck to play cards with"""
-    def __init__(self):
-        """Initialize deck as a list of all 52 cards:
-           13 cards in each of 4 suits"""
-        self.__deck = []
-        for rank in range(2,11):
-            for suit in ['H','C','D','S']:
-                card = Card(rank, suit)
-                self.__deck.append(card)
-        for rank in ['J', 'Q', 'K', 'A']:
-            for suit in ['H', 'C' , 'D' , 'S']:
-                card = Card(rank, suit)
-                self.__deck.append(card)
-
-    def shuffle(self):
-        """Shuffle the deck"""
-        random.shuffle(self.__deck)
-
-    def get_deck(self):
-        'Get deck'
-        return self.__deck
-
-    def deal(self):
-        'Deal the last card in the deck'
-        # get the last card in the deck
-        # simulates a pile of cards and getting the top one
-        return self.__deck[-1]
-            
-    def __str__(self):
-        """Represent the whole deck as a string for printing -- very useful during code development"""
-       #the deck is a list of cards
-       #this function just calls str(card) for each card in list
-       # put a '\n' between them
-        output_string = ''
-        output_string += 'deck is listed below: \n'
-        length = len(self.__deck)
-        for card in self.__deck:
-            output_string += str(card.rank) +card.suit + '\n'
-        return output_string
-
+from Cards import *
 
 class BlackJack():
     'Solitaire game'
@@ -79,9 +13,23 @@ class BlackJack():
         
     def initial_display(self):
         'Display the inital state of the game'
+        self.disposal = []
+        self.table = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        print table[0:5]
+        print table[5:10]
+        print ' ',table[10:13]
+        print ' ',table[13:16]
+        
 
     def make_a_move(self):
         'Allow user to make a move'
+        card = 0
+        print "The top card of the deck is", card
+        choice = raw_input('Do you want to put it into tableau?(Y or N):')
+        if choice in 'Yy':
+            slot=raw_input('which slot do you want to put it in: ')
+        else:
+            disposal.append(card)
 
     def current_display(self):
         'Display the current state of the game'
@@ -103,8 +51,19 @@ class BlackJack():
     
     def play(self):
         'Play Solitaire Game'
+        # initial display
         BlackJack.initial_display()
+
+        # shuffle deck
+        # deal a card
+        # allow user to make a move
         BlackJack.make_a_move()
+        # display current state of the game
+        # repeat above 3 steps (deal card, place card, display game)
+        # print a msg saying I am about to score the hand, pass table to score function
+        # final msg to display the score of table
+        # highest score display if user breaks the record
+        # restart choice
         #.........................................
 
 
@@ -113,15 +72,7 @@ def main():
     bj_solitaire = BlackJack()
     # display the initial state of the game
     bj.solitaire.play()
-    # shuffle deck
-    # deal a card
-    # allow user to make a move
-    # display current state of the game
-    # repeat above 3 steps (deal card, place card, display game)
-    # print a msg saying I am about to score the hand, pass table to score function
-    # final msg to display the score of table
-    # highest score display if user breaks the record
-    # restart choice
+
     
 
 if __name__ =="__main__":
