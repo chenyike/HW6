1a1e91fc0df71c9c80d373c824a5a18dce853288
chenyike <chenyike.upenn@gmail.com>
2015-02-22 17:56:10 -0500

    Revert be14f34..982158c
    This rolls back to commit be14f34df46bb5c33dbfa3fa76926d16874d45f6.

--- SoloBlackjack.py
+++ SoloBlackjack.py
@@ -7,23 +7,15 @@ class BlackJack():
     'Solitaire game'
     def __init__(self):
         'initialize BlackJack class'
-        self.disposal = []
-        self.table = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
     def get_value(self,card):
         'Get the value of the card'
         
     def initial_display(self):
         'Display the inital state of the game' 
-<<<<<<< HEAD
-
-        print 'The table looks like this, also, numbers on it mark the slots: \n',
-        print self.table[0:5]
-=======
         self.disposal = []
         self.table = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
         print 'The table looks like this, also, numbers on it mark the slots: \n',self.table[0:5]
->>>>>>> origin/master
         print self.table[5:10]
         print ' ',self.table[10:13]
         print ' ',self.table[13:16]
@@ -37,7 +29,7 @@ class BlackJack():
         if choice in 'Yy':
             slot=raw_input('which slot do you want to put it in: ')
         else:
-            self.disposal.append(card)
+            disposal.append(card)
 
     def current_display(self):
         'Display the current state of the game'
@@ -64,19 +56,13 @@ class BlackJack():
 
         # shuffle deck
         Deck().shuffle()
+ 
         
         # deal a card
-<<<<<<< HEAD
-        while len(Deck().get_deck())+len(self.disposal)>36:
-            card = Deck().deal()
-            self.make_a_move(card)
-            self.current_display()
-=======
         card = Deck().deal()
         
         # allow user to make a move
         self.make_a_move(card)
->>>>>>> origin/master
         
         # display current state of the game
         
